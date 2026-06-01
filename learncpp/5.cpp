060e3b9aac8048fc186d478d5480ad5052406402
Aditya Pratap Singh Bhadoria <abhadoria805@gmail.com>
2026-06-01 23:02:56 +0530

    Initial commit

---
+++ learncpp/5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/Adity/Downloads/CP/input.txt", "r", stdin);
+    freopen("/Users/Adity/Downloads/CP/output.txt", "w", stdout);
+    #endif
+    // <----------- 5.1 ------------>
+    int x { 4 }; // x is a non-constant variable
+    return 0;
+}
